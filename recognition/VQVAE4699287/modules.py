9ec4e31583d5d3992086927bff3e2dfa6fdda4ba
Declan Bolster <declanbolsterxyz@gmail.com>
2025-10-15 13:32:34 +1000

    Encoder completed.
    Encoder written, using same structure as traditional Variational VAE.

--- recognition/VQVAE4699287/modules.py
+++ recognition/VQVAE4699287/modules.py
@@ -1,9 +1,4 @@
-
-import torch
 import torch.nn as nn
-import torchvision
-import torchvision.transforms as transforms
-import time
 
 
 class VQVAE(nn.module):
@@ -11,13 +6,33 @@ class VQVAE(nn.module):
         super().__init()
         # Encoder
         self.enc_conv = nn.Sequential(
+
+            ## necessary image convolution
             nn.Conv2d(1, 32, 4, 2, 1), ## 64->32
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            ResidualBlock(32),
-            nn.Conv2d(32, 64, 4, 2, 1) ## 32->16
+
+            ## Dropout to prevent overfitting
+            nn.Dropout2d(0.2),
+
+            nn.Conv2d(32, 64, 4, 2, 1), ## 32->16
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            ResidualBlock(64),
-            nn.Conv2d(64, 128, 4, 2, 1), ## 
+            nn.Dropout2d(0.2),
+            
+
+            nn.Conv2d(64, 128, 4, 2, 1), ## 16->6
+            nn.BatchNorm2d(128),
+            nn.ReLU(), 
+            nn.Dropout2d(0.2),
+
+            nn.Conv2d(128, 256, 4, 2, 1), ## 8->4
+            nn.ReLU(),
+
+            ## need to check if this is necessary between
+            ## image generation
+            nn.Flatten(),
         )
+
+        ## this is the intermittent point where the 
+        ## codebook is implemented
