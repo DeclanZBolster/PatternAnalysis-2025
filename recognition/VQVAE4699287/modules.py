82268f566c444cac51e8b370915854236a621106
Declan Bolster <declanbolsterxyz@gmail.com>
2025-10-15 14:23:15 +1000

    Reconsidering VQVAE implementation:
    Considering reimplementing VAE portion into separate classes (Encoder
    and Decoder), as opposed to a single class.

--- recognition/VQVAE4699287/modules.py
+++ recognition/VQVAE4699287/modules.py
@@ -1,9 +1,25 @@
 import torch.nn as nn
 
 
+## Might be easier to have them as classes
+class Encoder(nn.module):
+    def __init__(self, z_dim=50):
+        super().__init()
+        self.z_dim = z_dim
+
+
+
+class Decoder(nn.module):
+    def __init__(self, z_dim=50):
+        super().__init()
+        self.z_dim = z_dim
+
+
+
 class VQVAE(nn.module):
     def __init__(self, z_dim=50):
         super().__init()
+        self.z_dim = z_dim
         # Encoder
         self.enc_conv = nn.Sequential(
 
@@ -36,3 +52,25 @@ class VQVAE(nn.module):
 
         ## this is the intermittent point where the 
         ## codebook is implemented
+
+        # Decoder
+        self.fc = nn.Linear(z_dim, 256*4*4)
+        self.dec_conv = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, 4, 2, 1), #4->8
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(128, 64, 4, 2, 1), #8->16
+            nn.BatchNorm2d(128), 
+            nn.ReLU(), 
+
+            nn.ConvTranspose2d(64, 32, 4, 2, 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 1, 4, 2, 1),
+
+            nn.Sigmoid()
+
+        )
+
+
